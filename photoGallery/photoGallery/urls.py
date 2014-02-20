f11b9116112bbddd902e9408f0002f41ec148fa4
Yakshaman <yakshaman@gmail.com>
2014-02-20 18:13:39 +0100

    changue url scheme

--- photoGallery/photoGallery/urls.py
+++ photoGallery/photoGallery/urls.py
@@ -7,7 +7,8 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'photoGallery.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    
+
+    url(r'', include('photos.urls')),
     url(r'^photos/', include('photos.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
