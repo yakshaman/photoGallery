f008f59deec57dbb68748cff48396ea16f824004
Yakshaman <yakshaman@gmail.com>
2014-02-20 18:14:25 +0100

    set some urls

--- photoGallery/photos/urls.py
+++ photoGallery/photos/urls.py
@@ -3,5 +3,9 @@ from django.conf.urls import patterns, url
 from photos import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^album/$', views.index, name='index'),
+    url(r'^photo/$', views.index, name='index'),
+    url(r'^album/(?P<album_id>\d+)/$', views.album, name='album'),
+    url(r'^photo/(?P<photo_id>\d+)/$', views.photo, name='photo'),
 )
